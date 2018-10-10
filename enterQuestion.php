3573f0cfe078bc4540791c5c6c04ad8ae9174ec0
jennyshand <44037552+jennyshand@users.noreply.github.com>
2018-10-10 12:42:15 -0700

    Update enterQuestion.php

--- enterQuestion.php
+++ enterQuestion.php
@@ -1,3 +1,12 @@
 <?php
-    function
+    function enterQuest()
+    {
+        ?>
+        <form method="post"
+                  action="<?= htmlentities($_SERVER['PHP_SELF'],
+                                   ENT_QUOTES) ?>">
+        <!–– ***OTHER HTML STUFF FOR GUI*** -->
+        <?php
+    }
+?>
 Hello Jenny
