7c6770db493b19e7a3e707b66b3f224bcd2fd0e8
jennyshand <44037552+jennyshand@users.noreply.github.com>
2018-10-31 12:36:28 -0700

    Update RonMatronDesign.sql

--- RonMatronDesign.sql
+++ RonMatronDesign.sql
@@ -14,72 +14,72 @@
  /* This table will consist of a repositiory of correct answers to common questions that will 
     be linked to questions through the keywords table. answer_id is a unique 4 digit code that 
     identifies each keyword.
-  */
+ */
   
-  drop table answer cascade constraints;
+ drop table answer cascade constraints;
   
-  create table answer
-  (answer_id   char(4),
-   answer_cat   varchar(20) not null,
-   answer_name  varchar(20) not null,
-   primary key (answer_id),
-   foreign key (answer_cat) references category (category_id)
-  );
+ create table answer
+ (answer_id   char(4),
+  answer_cat   varchar(20) not null,
+  answer_name  varchar(20) not null,
+  primary key (answer_id),
+  foreign key (answer_cat) references category (category_id)
+ );
    
-   /* This table will consist of categories of types of questions and answers (ie. answers
+ /* This table will consist of categories of types of questions and answers (ie. answers
       relating to printers would belong to a "printer" category, or questions asked about
       where to find certain software sets would be linked to a "software category")
-   */
+ */
    
-   drop table category cascade constraints;
+ drop table category cascade constraints;
    
-   create table category 
-   (category_id   char(4),
-    category_name   varchar(20) not null,
-    primary key (category_id),
-    foreign key (keyword_id) references keyword,
-    foreign key (answer_id) references answer
-   );
+ create table category 
+ (category_id   char(4),
+  category_name   varchar(20) not null,
+  primary key (category_id),
+  foreign key (keyword_id) references keyword,
+  foreign key (answer_id) references answer
+ );
     
-    /* This table will describe what answers may be relevant to specific keywords grabbed
-       from questions.
-    */
+ /* This table will describe what answers may be relevant to specific keywords grabbed
+    from questions.
+ */
+   
+ drop table keyword_has_answer cascade constraints;
     
-    drop table keyword_has_answer cascade constraints;
-    
-    create table keyword_has_answer
-    (keyword_id   char(6),
-     answer_id   char(4),
-     primary key (keyword_id, answer_id),
-     foreign key (keyword_id) references keyword,
-     foreign key (answer_id) references answer
-    );
+ create table keyword_has_answer
+ (keyword_id   char(6),
+  answer_id   char(4),
+  primary key (keyword_id, answer_id),
+  foreign key (keyword_id) references keyword,
+  foreign key (answer_id) references answer
+ );
        
-    /* This table will consist of a repository of feedback responses that the admins
-       can use to update relevant answers linked to keywords from questions */
+ /* This table will consist of a repository of feedback responses that the admins
+    can use to update relevant answers linked to keywords from questions */
       
-    drop table feedback cascade constraints;
+ drop table feedback cascade constraints;
     
-    create table feedback 
-    (feedback_id   char(4),
-     feedback_info   varchar(200) not null,
-     feedback_cat   char(6),
-     answer_id   char(4),
-     primary key (feedback_id),
-     foreign key (feedback_cat) references category (category_id),
-     foreign key (answer_id) references answer
-    );
+ create table feedback 
+ (feedback_id   char(4),
+  feedback_info   varchar(200) not null,
+  feedback_cat   char(6),
+  answer_id   char(4),
+  primary key (feedback_id),
+  foreign key (feedback_cat) references category (category_id),
+  foreign key (answer_id) references answer
+ );
      
-     /* This table links feedback to various keywords from questions asked by users for
-        admins to evaluate and implement, if necessary */
+ /* This table links feedback to various keywords from questions asked by users for
+    admins to evaluate and implement, if necessary */
      
-     drop table feedback_keywords cascade constraints;
+ drop table feedback_keywords cascade constraints;
      
-     create table feedback_keywords
-     (feedback_keywords_id  char(4),
-      keyword_id   char(6), 
-      feedback_id   char(4),
-      primary key (feedback_keywords_id),
-      foreign key (keyword_id) references keyword,
-      foreign key (feedback_id) references feedback
-     );
+ create table feedback_keywords
+ (feedback_keywords_id  char(4),
+  keyword_id   char(6), 
+  feedback_id   char(4),
+  primary key (feedback_keywords_id),
+  foreign key (keyword_id) references keyword,
+  foreign key (feedback_id) references feedback
+ );
