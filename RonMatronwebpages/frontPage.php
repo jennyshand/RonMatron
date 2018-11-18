63339dfd58bf207b124f21e1ce9b99270e8bc2af
jennyshand <44037552+jennyshand@users.noreply.github.com>
2018-11-17 17:35:30 -0800

    Update and rename frontPage.html to frontPage.php

---
+++ RonMatronwebpages/frontPage.php
@@ -0,0 +1,56 @@
+<?php
+    function create_FrontPage()
+    {
+        ?>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8" />
+			<meta http-equiv="X-UA-Compatible" content="IE=edge,chrom=1">
+			<meta name="robots" content="index,follow">
+			<link rel="stylesheet" type="text/css" href="RonMatronFront.css">
+			<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+			<title>Welcome to RonMatron</title>
+
+		</head>
+		<body>
+			<div class="container12">
+				<header class="header_topgfront">
+			<header class="header_bottomyfront"></header>
+			</header>
+				<header class="stuff">
+
+			<div> 
+				<img src="Ron.PNG" alt="Ron's Face">
+				</div>
+
+
+					<div class="row"> 
+						<div class="og">    
+						<div class="itmHolder">
+							<div class="title" >
+									<h1> Welcome to RonMatron </h1>
+						</div>
+								<div class="sub" id="header-nav">
+
+									<button class="one" type="button">Check the FAQ</button>
+                 
+									<button class="two" type="button">Ask the RonMatron</button>
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</header>
+				<div class="footer_topyfront">
+				<div class="footer_bottomgfront"></div>
+				</div>
+			</div>
+		</body>    
+		</html>
+		<?php
+    }
+?>
+
+
