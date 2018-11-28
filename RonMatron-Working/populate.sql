09c32ca19f8bd7a72d3e559dffab8f6df2615087
Emily Oparowski <35502919+Opaibiski@users.noreply.github.com>
2018-11-27 20:26:50 -0800

    Update populate.sql

--- RonMatron-Working/populate.sql
+++ RonMatron-Working/populate.sql
@@ -71,6 +71,32 @@ insert into keyword2
 values
 ('000005', '0005', 'Printer');
 
+insert into keyword2
+values
+('000006', '0002', 'Print');
+
+insert into keyword2
+values
+('000007', '0002', 'Kiosks');
+
+insert into keyword2
+values
+('000008', '0002', 'Located');
+
+insert into keyword2
+values
+('000009', '0005', 'add');
+
+insert into keyword2
+values
+('000010', '0005', 'money');
+
+insert into keyword2
+values
+('000011', '0005', 'c-card');
+
+
+
 
 prompt inserting rows into answer2
 prompt This is the insert for 7-Zip
@@ -103,6 +129,15 @@ insert into answer2
 values
 ('0005', '0006', 'its.humboldt.edu/computers-printers-phones/windows-wireless-printing-installation-students');
 
+prompt This insert is for where print kiosks are located
+insert into answer2
+values
+('0006','0002','Natural Resources, Forestry, Harry Griffith Hall, Gist Hall, The University Center, Founders Hall, Kineseology, and Library');
+
+insert into answer2
+values
+('0007','0005','Funds may be added online at c-card.humboldt.edu, at any cashier on campus for example: the Marketplace, Depot, or Library Cafe, or on the second floor of the library near the stairwell.');
+
 
 prompt insert into table keyword_has_answer
 
@@ -130,16 +165,40 @@ insert into keyword_has_answer2
 values
 ('000005','0005');
 
+insert into keyword_has_answer2 
+values
+('000006','0006');
+
+insert into keyword_has_answer2 
+values
+('000007','0006');
+
+insert into keyword_has_answer2 
+values
+('000008','0006');
+
+insert into keyword_has_answer2 
+values
+('000009','0007');
+
+insert into keyword_has_answer2 
+values
+('000010','0007');
+
+insert into keyword_has_answer2 
+values
+('000011','0007');
+
 
 prompt inserting into the feedback table 
 
 insert into feedback2
 values
-('0000','RonMatron was greatly helpful with helping me locate where python is located so I can finish my python homework.','0000', '0003');
+('0000','RonMatron was greatly helpful with helping me locate where python is located so I can finish my python homework.','0000', '0003',1, 'Kurt J. Miner','KurtMiner@gmail.com');
 
 insert into feedback2
 values
-('0001','RonMatron helped me figure out how to print to one of the wireless printers on campus.','0006', '0005');
+('0001','RonMatron helped me figure out how to print to one of the wireless printers on campus.','0006', '0005', 1, 'Virgil N. Bauder','Bauder@hotmail.com');
 
 prompt inserting into the feedback_keywords table
 
